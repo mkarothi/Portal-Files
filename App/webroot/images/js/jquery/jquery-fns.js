ced555bc55b4fd04bd0d4899c4a0097401f0f90a
mkarothi <37001053+mkarothi@users.noreply.github.com>
2018-08-12 00:48:35 +0530

    Add files via upload

--- App/webroot/images/js/jquery/jquery-fns.js
+++ App/webroot/images/js/jquery/jquery-fns.js
@@ -1,1 +1,13 @@
-
+$(function(){
+    $('.blink').
+        focus(function() {
+            if(this.title==this.value) {
+                this.value = '';
+            }
+        }).
+        blur(function(){
+            if(this.value=='') {
+                this.value = this.title;
+            }
+        });
+})
